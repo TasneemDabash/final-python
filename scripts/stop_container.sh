33135ef5fbe8b95552178c80e7d041c060e22392
TasneemDabash <89144127+TasneemDabash@users.noreply.github.com>
2024-04-28 19:29:58 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,10 @@
-docker stop final-python
-docker rm final-python
+
+sudo docker ps -a | grep final-python
+EXITCODE=$?
+if [ "$EXITCODE" -ne "0" ]; then
+    echo "uh oh"
+    exit 0
+else
+    docker stop final-python
+    docker rm final-python
+fi
